71b59cdf9756dcd7a656faca36f880208fdba536
Dave Tarrant <davetaz@gmail.com>
2020-03-05 09:30:01 +0000

    Grid, number line and random.

--- js/interact.js
+++ js/interact.js
@@ -1,37 +1,83 @@
-var element = document.getElementById('number1');
-var element2 = document.getElementById('number2');
-var element3 = document.getElementById('number3');
-var element4 = document.getElementById('number4');
-var element5 = document.getElementById('number5');
-var element6 = document.getElementById('number6');
-var element7 = document.getElementById('number7');
-makeInteractive(element,0,0);
-makeInteractive(element2,180,0);
-makeInteractive(element3,360,0);
-makeInteractive(element4,540,0);
-makeInteractive(element5,720,0);
-makeInteractive(element6,900,0);
-makeInteractive(element7,0,100);
+var numbers = [ 33750, 33750, 33750, 33750, 44000, 44000, 44000, 45566, 65000, 95000, 103500, 112495, 138188, 141666, 181500, 185000, 190000, 194375, 195000, 205000, 292500, 301999, 4600000, 5600000 ];
+var rows = 6;
+var cols = 5;
+var max_rows = 6;
+var max_cols = 7;
 
-$("#button").click(function(){
-    putBack('number1',0,0);
-    putBack('number2',180,0);
-    putBack('number3',360,0);
-    putBack('number4',540,0);
-    putBack('number5',720,0);
-    putBack('number6',900,0);
-    putBack('number7',0,100);
+addElements(numbers);
+drawRandom(numbers,max_rows,max_cols);
+
+$("#showGrid").click(function(){
+    rows = 6;
+    cols = 6;
+    reDrawGrid(numbers,rows,cols);
 });
 
-function putBack(element,x,y) {
+$("#showNumberLine").click(function(){
+    rows = 1;
+    cols = 1000;
+    reDrawGrid(numbers,rows,cols);
+});
+
+function addElements(numbers) {
+  $.each(numbers, function(idx) {
+    $("body").append('<div id="number'+idx+'" class="number">'+numbers[idx]+'</div>'); 
+  });
+}
+
+function drawRandom(numbers,max_rows,max_cols) {
+  var done = {};
+  $.each(numbers, function(idx) {
+    var row = Math.floor(Math.random() * max_rows);
+    var col = Math.floor(Math.random() * max_cols);
+    var key = row + ',' + col;
+    while (done[key]) {
+      var row = Math.floor(Math.random() * max_rows);
+      var col = Math.floor(Math.random() * max_cols);
+      var key = row + ',' + col;
+    }
+    done[key] = "true";
+    var y = 100 * row;
+    var x = 180 * col;
+    var element = document.getElementById('number'+idx);
+    makeInteractive(element,x,y);
+  });
+}
+
+function drawGrid(numbers,rows,cols) {
+  var row = 0;
+  $.each(numbers, function(idx) {
+    if (idx % cols == 0 && idx > 0) {
+      row += 1;
+    }
+    var y = 100 * row;
+    var x = 180 * (idx % cols);
+    var element = document.getElementById('number'+idx);
+    makeInteractive(element,x,y);
+  });
+}
+
+function putBackGrid(element,x,y) {
     var transitionEvent = whichTransitionEvent();
     $("#"+element).css('transition','all 0.3s linear');
     $("#"+element).css('transform','translate(' + x + 'px, ' + y + 'px)');
     $("#"+element).one(transitionEvent, function(event) {
-      console.log('transition complete');
       var topass = document.getElementById(element);
       makeInteractive(topass,x,y);
     });
+}
+
+function reDrawGrid(numbers,rows,cols) {
+  var row = 0;
+  $.each(numbers, function(idx) {
+    if (idx % cols == 0 && idx > 0) {
+      row += 1;
+    }
+    var y = 100 * row;
+    var x = 180 * (idx % cols);
+    putBackGrid('number'+idx,x,y);
+  });
+  setTimeout(function () { drawGrid(numbers,rows,cols);},1000);
 }
 
 function whichTransitionEvent(){
